a29abc9346ed600634695744ec768af178bc14dc
luvlsco <lucas.alan.vlsco@gmail.com>
2024-11-20 11:57:54 -0300

    chore: ordenando archivos

--- src/images.py
+++
@@ -1,5 +0,0 @@
-import pygame
-
-pygame.init()
-
-utn_icono = pygame.image.load("assets/images/icons/icono_utn.png")
--- src/sounds.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-pygame.init()
-pygame.mixer.init()
-
-# Sonido (sfx) (WAV o OGG)
-sonido_apertura = pygame.mixer.Sound("assets/sounds/sfx/abrir_juego.wav")
-sonido_apertura.set_volume(0.65)
-sonido_apertura_reproducido = False
